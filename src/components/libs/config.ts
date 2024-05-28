30bfb464f88c9fd7f058eda5e6f9a81c42a87a86
athulAnilkumarAppu <athulanilkumarappu@gmail.com>
2024-05-28 18:03:41 +0530

    change

--- src/components/libs/config.ts
+++ src/components/libs/config.ts
@@ -1,2 +1,3 @@
-export const BASE_URl = 'https://pixelz-wallpapers-server.onrender.com/'
+// export const BASE_URl = 'https://pixelz-wallpapers-server.onrender.com/'
 // export const BASE_URl = 'http://localhost:3000/'
+export const BASE_URl = 'https://pixelz-wallpapers-server-1.onrender.com'
